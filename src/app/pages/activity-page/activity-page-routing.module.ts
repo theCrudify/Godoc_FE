1207104209537214060f87a4d2a5f968ff249b0f
Jidan Fatahillah <jidanfatahillah2003@gmail.com>
2025-03-26 00:45:18 +0700

    feat: adding activity

--- src/app/pages/activity-page/activity-page-routing.module.ts
+++ src/app/pages/activity-page/activity-page-routing.module.ts
@@ -1,12 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListDocumentNumberComponent } from './01-document-numbers/list-document-number/list-document-number.component';
 
-import { DocumentListComponent } from './documents/document-list/document-list.component';
-import { DocumentCreateComponent } from './documents/document-create/document-create.component';
-import { DocumentDetailComponent } from './documents/document-detail/document-detail.component';
-import { ApprovalFormComponent } from './approvals/approval-form/approval-form.component';
-import { ProductMaintenanceAddComponent } from './product_packaging_maintenance/product-maintenance-add/product-maintenance-add.component';
-import { ProductMaintenaceComponent } from './product_packaging_maintenance/product-maintenace/product-maintenace.component';
+
 const activityPageRoutes: Routes = [
   // {
   //   path: 'signin', loadChildren: () => import('./auth/signin/signin.module').then(m => m.SigninModule)
@@ -14,38 +10,6 @@ const activityPageRoutes: Routes = [
 
   //Activity
 
-  {
-    path: 'document-list',
-    component: DocumentListComponent,
-  },
- 
-  {
-    path: 'document-create',
-    component: DocumentCreateComponent,
-  },
-
-  {
-    path: 'document-detail',
-    component: DocumentDetailComponent,
-  },
-
-
-
-  {
-    path: 'product-maintenance-add',
-    component: ProductMaintenanceAddComponent,
-  },
-
-  {
-    path: 'product-maintenace',
-    component: ProductMaintenaceComponent,
-  },
-
-
-
-
-
-
 
 
 
@@ -55,8 +19,8 @@ const activityPageRoutes: Routes = [
 
 
   {
-    path: 'approval-form',
-    component: ApprovalFormComponent,
+    path: "document-number",
+    component: ListDocumentNumberComponent,
   },
 ];
 
--- src/app/pages/activity-page/activity-page.module.ts
+++ src/app/pages/activity-page/activity-page.module.ts
@@ -33,28 +33,18 @@ import { MasterDataModule } from '../master-data/master-data.module';  // Impor 
 import { NgxLoadingModule } from 'ngx-loading';
 import { SharedModule } from '../../shared/shared.module';
 import { ActivityPageRoutingModule } from './activity-page-routing.module';
-import { DocumentListComponent } from './documents/document-list/document-list.component';
-import { DocumentCreateComponent } from './documents/document-create/document-create.component';
-import { DocumentDetailComponent } from './documents/document-detail/document-detail.component';
-import { ApprovalFormComponent } from './approvals/approval-form/approval-form.component';
-import { ProductMaintenanceAddComponent } from './product_packaging_maintenance/product-maintenance-add/product-maintenance-add.component';
-import { ProductMaintenaceComponent } from './product_packaging_maintenance/product-maintenace/product-maintenace.component';
+import { ListDocumentNumberComponent } from './01-document-numbers/list-document-number/list-document-number.component';
+
 @NgModule({
   declarations: [
 
 
 
     //Activity
-    DocumentListComponent,
-    DocumentCreateComponent,
-    DocumentDetailComponent,
 
 
+    ListDocumentNumberComponent,
 
-    ApprovalFormComponent,
-
-    ProductMaintenanceAddComponent,
-    ProductMaintenaceComponent
   ],
   imports: [
     CommonModule,
